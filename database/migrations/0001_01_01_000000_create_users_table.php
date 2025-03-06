c2ae205ea76a598e42ec7197c024e0a4b243a123
BNohut <bugra.nohut@gmail.com>
2025-03-06 22:26:11 +0300

    Task6: Require Sanctum Package for login logic

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('uuid')->primary()->default(DB::raw('(UUID())'));
             $table->string('name');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
