593f55b94c787ae9d6d7ebf8ecc4e93cd5f1d07a
BNohut <bugra.nohut@gmail.com>
2025-03-06 21:45:55 +0300

    Task4: Fix and Modify Database Migration Files

--- database/migrations/2025_03_06_153810_create_addresses_table.php
+++ database/migrations/2025_03_06_153810_create_addresses_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('addresses', function (Blueprint $table) {
-            $table->uuid('uuid')->primary()->unique();
+            $table->uuid('uuid')->primary()->unique()->default(DB::raw('(UUID())'));
             $table->uuid('user_uuid');
             $table->string('title');
             $table->string('address_line1');
